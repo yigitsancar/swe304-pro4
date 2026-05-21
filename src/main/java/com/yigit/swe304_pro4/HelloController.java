5dad323bc1b0b139fb30ab2c1fc95ecb30e5b00e
yigitsancar <ygtsncr07@gmail.com>
2026-05-21 11:34:46 +0300

    test99

--- src/main/java/com/yigit/swe304_pro4/HelloController.java
+++ src/main/java/com/yigit/swe304_pro4/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello SWE304 Project 1 alperen";
+        return "Hello SWE304 Project 1 Yigit";
     }
 }
